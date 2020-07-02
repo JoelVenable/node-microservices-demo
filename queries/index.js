4d633f0850f55c4f0428b1beeaf38f24c1096ce7
Joel Venable <joel.d.venable@gmail.com>
2020-07-02 15:13:02 +0000

    Added query service

---
+++ queries/index.js
@@ -0,0 +1,43 @@
+
+const express = require('express');
+// const { v4: uuid } = require('uuid');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+const app = express();
+app.use(bodyParser.json())
+app.use(cors());
+
+
+const posts = {};
+
+
+app.get('/posts', (req, res) => {
+    res.send(posts)
+})
+
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+
+    if (type === 'PostCreated') {
+        const { id, title } = data;
+
+        posts[id] = { id, title, comments: [] };
+    }
+
+    if (type === 'CommentCreated') {
+        const { id, postId, content } = data;
+
+        const post = posts[postId]
+        post.comments.push({ id, content });
+    }
+
+    console.log(posts)
+
+    res.status(204).send();
+})
+
+app.listen(4002, () => {
+    console.log('listening on port 4002')
+})
